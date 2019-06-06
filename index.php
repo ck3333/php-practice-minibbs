c9957401dfaaf0df760965b6a5f4587b065e6842
moyashi3 <hs333.bz@gmail.com>
2019-06-06 20:55:01 +0900

    6-13

--- index.php
+++ index.php
@@ -41,7 +41,24 @@ if (!empty($_POST)) {
 }
 
 // 投稿を取得する
-$posts = $db->query('SELECT m.name, m.picture, p.* FROM members m, posts p WHERE m.id=p.member_id ORDER BY p.created DESC');
+if ( isset($_GET['page']) ) {
+  $page = $_GET['page'];
+} else {
+  $page = 1;
+}
+$page = max($page, 1); //URLパラメータにマイナス値が指定された場合の対策
+
+// 最終ページを取得
+$counts = $db->query('SELECT COUNT(*) AS cnt FROM posts');
+$cnt = $counts->fetch();
+$maxPage = ceil($cnt['cnt'] / 5);//切り上げ
+$page = min($page, $maxPage);//最大ページを超えることはない
+
+$start = ($page - 1) * 5;
+
+$posts = $db->prepare('SELECT m.name, m.picture, p.* FROM members m, posts p WHERE m.id=p.member_id ORDER BY p.created DESC LIMIT ?, 5');
+$posts->bindParam(1, $start, PDO::PARAM_INT);
+$posts->execute();
 
 // 返信の場合
 if ( isset($_GET['res']) ) {
@@ -55,6 +72,13 @@ if ( isset($_GET['res']) ) {
 // htmlspecialcharsのショートカット
 function h($value) {
   return htmlspecialchars($value, ENT_QUOTES);
+}
+
+// 本文内のURLにリンクを設定
+function makeLink($value) {
+  $pattern = "(https?)(://[[:alnum:]\+\$\;\?\.%,!#~*/:@&=_-]+)";
+  $replacement = '<a href="\1\2" target="_blank">\1\2</a>';
+  return mb_ereg_replace($pattern, $replacement, $value);
 }
 
 ?>
@@ -78,16 +102,16 @@ function h($value) {
   </div>
   <div id="content">
 
-    <form action="" method="post">
+    <div style="text-align: right"><a href="logout.php">ログアウト</a></div>
 
+    <form action="" method="post">
       <dl>
-        <dt><?php echo htmlspecialchars($member['name'], ENT_QUOTES); ?>さん、メッセージをどうぞ</dt>
+        <dt><?php echo h($member['name']); ?>さん、メッセージをどうぞ</dt>
         <dd>
           <textarea name="message" cols="50" rows="5"><?php if(isset($message)): echo h($message); endif; ?></textarea>
           <input type="hidden" name="reply_post_id" value="<?php echo h($_GET['res']); ?>">
         </dd>
       </dl>
-
       <div><input type="submit" value="投稿する"></div>
     </form>
 
@@ -95,23 +119,36 @@ function h($value) {
     <div class="msg">
       <img src="member_picture/<?php echo h($post['picture']); ?>" wudtg="48" height="48" alt="<?php echo h($post['name']); ?>">
       <p>
-        <?php echo h($post['message']); ?>
+        <?php echo makeLink(h($post['message'])); ?>
         <span class="name">(<?php echo h($post['name']); ?>)</span>
         [<a href="index.php?res=<?php echo h($post['id']); ?>">Re</a>]
       </p>
       <p class="day">
-        <a href="view.php?id=<?php echo h($post['id']); ?>">
-          <?php echo h($post['created']);  ?>
-        </a>
+        <a href="view.php?id=<?php echo h($post['id']); ?>"><?php echo h($post['created']); ?></a>
         <?php if ($post['reply_post_id'] > 0): ?>
         |
-        <a href="view.php?id=<?php echo h($post['reply_post_id']); ?>">
-          返信元のメッセージへ
-        </a>
+        <a href="view.php?id=<?php echo h($post['reply_post_id']); ?>">返信元のメッセージへ</a>
+        <?php endif; ?>
+        <?php if ( $_SESSION['id'] == $post['member_id'] ) : //自分の投稿だけを削除できるように ?>
+        [<a href="delete.php?id=<?php echo h($post['id']); ?>" style="color: #f33;">削除</a>]
         <?php endif; ?>
       </p>
     </div>
     <?php endforeach; ?>
+
+    <ul class="paging">
+      <?php if ($page > 1) : ?>
+      <li><a href="index.php?page=<?php echo $page - 1; ?>">前のページへ</a></li>
+      <?php else : ?>
+      <li>前のページへ</li>
+      <?php endif; ?>
+
+      <?php if ($page < $maxPage) : ?>
+      <li><a href="index.php?page=<?php echo $page + 1; ?>">次のページへ</a></li>
+      <?php else : ?>
+      <li>次のページへ</li>
+      <?php endif; ?>
+    </ul>
 
   </div>
 
